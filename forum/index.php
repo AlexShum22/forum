a40a5cb5e10ee5965a5fc3806d6921659f5e1904
AlexShum22 <sashum29@yandex.ru>
2018-04-28 16:46:42 +0300

    more

--- forum/index.php
+++ forum/index.php
@@ -5,6 +5,7 @@ class First
 	public $hello = "cho";
 	public $name;
 	public $sn;
+	private $exte = 300;
 	public function __construct ($name, $sn)
 	{
 		$this->name=$name;
@@ -12,12 +13,22 @@ class First
 	}
 	public function escape ()
 	{
-		echo $this->name . ' ' . $this->hello . ' ' . $this->sn;
+		echo $this->name . ' ' . $this->hello . ' ' . $this->sn . $this->exte;
 	}
 }
 
 $obj = new First('My', 'PC');
-echo $obj->hello;
-var_dump($obj);
 $obj->escape();
+
+class Second extends First
+{
+	public $en = First::$exte;
+	public function ect()
+	{
+		echo '<br> 123 ' . $this->name .  ' ' . $this->sn . $this->en;
+	}
+
+}
+$obj2 = new Second ('new', 'class');
+$obj2->ect();
 ?>
