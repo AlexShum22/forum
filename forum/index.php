2c6b68160106adbb6fb5887e7915fe47edc7a47a
AlexShum22 <sashum29@yandex.ru>
2018-04-28 14:22:18 +0300

    fourth

--- forum/index.php
+++ forum/index.php
@@ -1,8 +1,23 @@
 <?php
 
-class
+class First
 {
-	public $
+	public $hello = "cho";
+	public $name;
+	public $sn;
+	public function __construct ($name, $sn)
+	{
+		$this->name=$name;
+		$this->sn=$sn;
+	}
+	public function escape ()
+	{
+		echo $this->name . ' ' . $this->hello . ' ' . $this->sn;
+	}
 }
 
+$obj = new First('My', 'PC');
+echo $obj->hello;
+var_dump($obj);
+$obj->escape();
 ?>
